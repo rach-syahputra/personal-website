5baf6049b1b0847c106fcf6b282741ec8ac2fd9a
rach-syahputra <rach.syahputra@gmail.com>
2024-12-01 17:55:19 +0800

    add badge component

---
+++ src/components/badge.tsx
@@ -0,0 +1,26 @@
+import { Colors } from '@/lib/types'
+import { cn } from '@/lib/utils'
+
+interface BadgeProps {
+  color?: Colors
+  children: React.ReactNode
+}
+
+export default function Badge({ color, children }: BadgeProps) {
+  return (
+    <span
+      className={cn(
+        'h-6 content-center rounded-full bg-neutral-700 bg-opacity-30 px-2 text-xs text-neutral-500',
+        {
+          'bg-blue-950 text-blue-500': color === 'blue',
+          'bg-purple-950 text-purple-500': color === 'purple',
+          'bg-yellow-950 text-yellow-500': color === 'yellow',
+          'bg-green-950 text-green-500': color === 'green',
+          'bg-red-950 text-red-500': color === 'red'
+        }
+      )}
+    >
+      {children}
+    </span>
+  )
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,1 @@
+export type Colors = 'default' | 'blue' | 'purple' | 'yellow' | 'green' | 'red'
