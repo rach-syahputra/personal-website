7de81c063e5ac3ee0d1d6112b845a6b1d7bcd5eb
rach-syahputra <rach.syahputra@gmail.com>
2024-12-04 15:50:47 +0800

    adjust header component text size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,8 @@ interface HeaderProps {
 
 export default function Header({ children }: HeaderProps) {
   return (
-    <h1 className='text-center text-5xl font-bold lg:text-6xl'>{children}</h1>
+    <h1 className='text-center text-4xl font-bold md:text-5xl lg:text-6xl'>
+      {children}
+    </h1>
   )
 }
