3fffd312756bd90055f93982ad4e584f91cc00da
rach-syahputra <rach.syahputra@gmail.com>
2024-12-06 02:21:41 +0800

    update hero descriptions

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,4 +1,4 @@
-import { Github, Instagram, Mail } from 'lucide-react'
+import { Github, Instagram, Linkedin, Mail } from 'lucide-react'
 import Link from 'next/link'
 import Description from '@/components/description'
 import Header from '@/components/header'
@@ -8,28 +8,43 @@ import HorizontalLine from '@/components/horizontal-line'
 
 export default function Hero() {
   return (
-    <Section id='home' className='relative h-screen py-32 lg:py-0'>
-      <div className='shadow-photo relative h-36 w-36 overflow-hidden rounded-full bg-dark-hover bg-opacity-10 lg:h-[200px] lg:w-[200px]'>
+    <Section id='home' className='relative py-32 lg:py-0'>
+      <div className='relative h-36 w-36 overflow-hidden rounded-full bg-dark-hover bg-opacity-10 shadow-photo lg:h-[200px] lg:w-[200px]'>
         <Image src='/professional-photo.png' alt='professional photo' fill />
       </div>
-      <Header>I&rsquo;m Rachmat Syahputra</Header>
+      <div className='flex flex-col gap-2'>
+        <Description>Hello, my name is</Description>
+        <Header>Rachmat Syahputra</Header>
+      </div>
+
       <HorizontalLine animationDelay='1050' />
 
       <Description className='px-8'>
-        A <span className='text-white'>full-stack web developer</span> who work
-        with JavaScript Ecosystem and present high-quality modern web
+        I am a{' '}
+        <span className='font-bold text-light'>full-stack web developer</span>{' '}
+        who work with JavaScript Ecosystem and present high-quality modern web
         application.
       </Description>
       <HorizontalLine animationDelay='1100' />
-      <div className='flex gap-6'>
+      <div className='flex gap-8 md:gap-6'>
         <Link href='https://github.com/rach-syahputra/' target='_blank'>
-          <Github color='#888888' />
+          <Github color='#888888' size={28} className='lg:hidden' />
+          <Github color='#888888' size={32} className='hidden lg:block' />
+        </Link>
+        <Link
+          href='https://www.linkedin.com/in/rachmat-syahputra/'
+          target='_blank'
+        >
+          <Linkedin color='#888888' size={28} className='lg:hidden' />
+          <Linkedin color='#888888' size={32} className='hidden lg:block' />
         </Link>
         <Link href='mailto:rach.syahputra@gmail.com' target='_blank'>
-          <Mail color='#888888' />
+          <Mail color='#888888' size={28} className='lg:hidden' />
+          <Mail color='#888888' size={32} className='hidden lg:block' />
         </Link>
         <Link href='https://www.instagram.com/rach.syahputra/' target='_blank'>
-          <Instagram color='#888888' />
+          <Instagram color='#888888' size={28} className='lg:hidden' />
+          <Instagram color='#888888' size={32} className='hidden lg:block' />
         </Link>
       </div>
     </Section>
